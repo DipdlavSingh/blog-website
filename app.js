5df1e734d9feb351ab11bbaa2310e5f12a49d40a
DipdlavSingh <43384145+DipdlavSingh@users.noreply.github.com>
2020-05-21 02:21:37 +0530

    setup for atlas

--- app.js
+++ app.js
@@ -95,6 +95,6 @@ let port = process.env.PORT;
 if (port == null || port == '') {
   port = 3000;
 }
-app.listen(3000, function() {
+app.listen(port, function() {
   console.log("listening on port " + port);
 });
